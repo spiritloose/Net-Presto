bf1dbd0606b5cf42bcd8e58fbb371adfe3bf8981
Jiro Nishiguchi <jiro@cpan.org>
2015-03-16 20:13:12 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Net::Presto - It's new $module
+
+# SYNOPSIS
+
+    use Net::Presto;
+
+# DESCRIPTION
+
+Net::Presto is ...
+
+# LICENSE
+
+Copyright (C) Jiro Nishiguchi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Jiro Nishiguchi <jiro@cpan.org>
---
+++ lib/Net/Presto.pm
@@ -0,0 +1,39 @@
+package Net::Presto;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Net::Presto - It's new $module
+
+=head1 SYNOPSIS
+
+    use Net::Presto;
+
+=head1 DESCRIPTION
+
+Net::Presto is ...
+
+=head1 LICENSE
+
+Copyright (C) Jiro Nishiguchi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Jiro Nishiguchi E<lt>jiro@cpan.orgE<gt>
+
+=cut
+
